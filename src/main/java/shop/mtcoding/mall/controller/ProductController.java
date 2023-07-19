f99452f21f516770cf6f9ab0b285a47a58e632cc
unjoo31 <unjoo31@gmail.com>
2023-07-19 21:01:34 +0900

    20230719_3

--- src/main/java/shop/mtcoding/mall/controller/ProductController.java
+++ src/main/java/shop/mtcoding/mall/controller/ProductController.java
@@ -11,14 +11,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
+// @Controller 어노테이션은 Spring Framework에서 컨트롤러 클래스를 지정하는 어노테이션입니다.
+// 이 클래스가 컨트롤러 역할을 수행하며, 웹 요청에 대한 처리를 담당합니다.
 @Controller
 public class ProductController {
 
+    // @Autowired 어노테이션은 Spring Framework에서 의존성 주입(Dependency Injection)을 수행할 때 사용됩니다.
     @Autowired
     private ProductRepository productRepository;
 
     // 메인 페이지를 get해서 읽어줘
+    // @GetMapping 어노테이션은 HTTP GET 요청을 처리하는 메서드를 지정하는 어노테이션입니다.
     @GetMapping("/")
+    // HttpServletRequest 객체를 매개변수로 받고, ProductRepository를 사용하여 모든 상품 정보를 조회한 후,
+    // 조회한 결과를 "productlist"라는 이름으로 request의 attribute에 저장하고, "home"이라는 뷰를 반환합니다.
     public String home(HttpServletRequest request){
         List<Product> productList = productRepository.findAll();
         // 데이터는 먼저 request에 담는다.
@@ -28,13 +34,18 @@ public class ProductController {
     }
 
     // write 페이지를 get해서 읽어줘
+    //  "/write" 경로로 들어오는 GET 요청을 처리합니다. writePage() 메서드는 단순히 "write"라는 뷰를 반환합니다.
     @GetMapping("/write")
     public String writePage(){
         return "write";
     }
 
     // product를 post해서 줄게
+    // "/product" 경로로 들어오는 POST 요청을 처리합니다.
     @PostMapping("/product")
+    // write() 메서드는 name, price, qty라는 세 개의 매개변수를 받고,
+    // 이를 출력한 후 ProductRepository를 사용하여 데이터베이스에 새로운 상품 정보를 저장합니다.
+    // 그리고 "/" 경로로 리다이렉트하여 상품 리스트를 다시 조회하도록 합니다.
     public String write(String name, int price, int qty){
         System.out.println("name : " + name);
         System.out.println("price : " + price);
--- src/main/java/shop/mtcoding/mall/model/ProductRepository.java
+++ src/main/java/shop/mtcoding/mall/model/ProductRepository.java
@@ -12,20 +12,29 @@ import java.util.List;
 public class ProductRepository {
 
     @Autowired // IoC 컨테이너에서 new되어 있는걸 꺼내온다 (디펜던씨 인젝션)
+    // EntityManager : JPA(Java Persistence API)에서 가장 중요한 인터페이스 중 하나로,
+    // 영속성 컨텍스트(Persistence Context)와 데이터베이스 간의 상호작용을 관리하는 역할을 한다
+    // JPA 엔티티와 데이터베이스 사이의 매핑을 처리하고 데이터베이스에 대한 CRUD(Create, Read, Update, Delete) 작업을 수행합니다.
     private EntityManager em;
 
     @Transactional // Transactional :다 알아서 transaction 시작하고 종료함
     public void save(String name, int price, int qty){
+        // 이 메서드는 Native SQL 쿼리를 생성하기 위해 EntityManager를 사용합니다.
+        // Native SQL 쿼리는 직접 SQL 문법을 사용하여 데이터베이스와 상호작용하는 방식입니다.
         Query query = em.createNativeQuery("insert into product_tb(name, price, qty) values(:name, :price, :qty)");
+        // 이 메서드를 통해 Native SQL 쿼리에 파라미터를 바인딩합니다.
+        // :name, :price, :qty와 같은 이름을 가진 파라미터를 쿼리에 설정하고, 실제 값은 메서드의 매개변수로부터 받아와 설정합니다.
         query.setParameter("name", name);
         query.setParameter("price", price);
         query.setParameter("qty", qty);
+        // 이 메서드를 호출하여 Native SQL 쿼리를 실행하고 데이터베이스에 영향을 미치는 작업을 수행합니다.
+        // 위의 코드에서는 데이터를 삽입(insert)하는 작업을 수행하고 있습니다.
         query.executeUpdate();
     }
 
     public List<Product> findAll() {
         Query query = em.createNativeQuery("select * from product_tb", Product.class);
-        // 여러건일때 : getResultList, 한건일때 : getsingle
+        // getResultList() 메서드는 JPA(Java Persistence API)에서 사용되는 메서드 중 하나로, 쿼리를 실행하여 데이터베이스에서 가져온 결과를 리스트로 반환합니다.
         List<Product> productList = query.getResultList();
         return productList;
     }
