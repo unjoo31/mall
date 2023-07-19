ae856cbfe2dbeb73890475f8b1b45a355e46cb53
unjoo31 <unjoo31@gmail.com>
2023-07-19 20:28:02 +0900

    20230719_2

--- src/main/java/shop/mtcoding/mall/MallApplication.java
+++ src/main/java/shop/mtcoding/mall/MallApplication.java
@@ -3,11 +3,14 @@ package shop.mtcoding.mall;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 어노테이션을 통해 스프링 Bean을 읽어와 자동으로 생성해준다.
+// 이 어노테이션이 있는 파일 위치부터 설정들을 읽어가므로, 반드시 프로젝트의 최상단에 만들어야 한다.
 @SpringBootApplication
 public class MallApplication {
 
     public static void main(String[] args) {
+        // SpringApplication.run() 으로 해당 클래스를 run하면 내장 WAS를 실행한다.
+        // 내장 WAS의 장점으로는 개발자가 따로 톰캣과 같은 외부 WAS를 설치 후 설정해두지 않아도 애플리케이션을 실행할 수 있다.
         SpringApplication.run(MallApplication.class, args);
     }
-
 }
--- src/main/java/shop/mtcoding/mall/model/Product.java
+++ src/main/java/shop/mtcoding/mall/model/Product.java
@@ -7,10 +7,17 @@ import javax.persistence.*;
 
 @Getter
 @Setter
+// 테이블 생성시 만들어지는 테이블 이름 설정한다.
 @Table(name = "product_tb")
+// JPA에서 사용되며, JPA Entity로 지정한다.
+// JPA Entity는 데이터베이스 테이블과 매핑되는 클래스를 의미하며, 이를 통해 데이터베이스와 객체 지향 프로그래밍간의 매핑을 지원한다.
 @Entity
 public class Product {
+    // JPA에서 사용되며, 해당 어노테이션이 붙을 필드를 테이블의 pk로 지정한다.
     @Id
+    // JPA에서 사용되며, 해당 어노테이션이 붙을 필드의 값을 자동으로 생성해주는 방법을 지정한다.
+    // strategy 속성을 통해 생성 전략을 설정할 수 있으며,
+    // 위의 예시에서는 GenerationType.IDENTITY를 사용하여 데이터베이스의 자동 증가(primary key auto-increment) 기능을 이용하여 기본 키 값을 자동으로 생성합니다.
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
